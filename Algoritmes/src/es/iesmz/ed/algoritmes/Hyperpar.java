c1ae97ec611ea249ba850efd29756d0c58f20079
joavidbui <joavidbui@alu.edu.gva.es>
2023-05-22 17:51:13 +0200

    First Commit

---
+++ Algoritmes/src/es/iesmz/ed/algoritmes/Hyperpar.java
@@ -0,0 +1,15 @@
+package es.iesmz.ed.algoritmes;
+
+public class Hyperpar {
+
+    private final long numero;
+
+    public Hyperpar(long numero) {
+        this.numero = numero;
+    }
+
+    public boolean esHyperPar() {
+        boolean esHyperPar = false;
+        return esHyperPar;
+    }
+}
---
+++ Algoritmes/src/es/iesmz/ed/algoritmes/HyperparTest.java
@@ -0,0 +1,12 @@
+package es.iesmz.ed.algoritmes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HyperparTest {
+
+    @Test
+    void esHyperPar() {
+    }
+}
