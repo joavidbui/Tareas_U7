d209effa3b5b2821fd4ea621a9830c9d92b95307
joavidbui <joavidbui@alu.edu.gva.es>
2023-05-22 18:31:57 +0200

    DividirFactorial -> Documentation

--- Algoritmes/src/es/iesmz/ed/algoritmes/DividirFactorial.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/DividirFactorial.java
@@ -1,5 +1,9 @@
 package es.iesmz.ed.algoritmes;
 
+/**
+ * Clase que contiene dos atributos, numerador y denominador; y una función para calcular el resultado de la división
+ * de sus factoriales.
+ */
 public class DividirFactorial {
     int numerador;
     int denominador;
@@ -9,6 +13,10 @@ public class DividirFactorial {
         this.denominador = denominador;
     }
 
+    /**
+     * Calcula el resultado de dividir el factorial del numerador por el factorial del denominador.
+     * @return int
+     */
     public int calcula() {
         System.out.println(numerador + " " + denominador);
         int res = 1;
