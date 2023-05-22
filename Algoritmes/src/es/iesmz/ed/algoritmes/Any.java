0f2fdb872d467506ddff55225c60271af4bd16e4
joavidbui <joavidbui@alu.edu.gva.es>
2023-05-22 17:56:10 +0200

    Any -> First Commit

---
+++ Algoritmes/src/es/iesmz/ed/algoritmes/Any.java
@@ -0,0 +1,14 @@
+package es.iesmz.ed.algoritmes;
+
+public class Any {
+    long numero;
+
+    public Any(long numero) {
+        this.numero = numero;
+    }
+
+    public int digitsDiferents(){
+        int res = 0;
+        return res;
+    }
+}
---
+++ Algoritmes/src/es/iesmz/ed/algoritmes/AnyTest.java
@@ -0,0 +1,7 @@
+package es.iesmz.ed.algoritmes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnyTest {
+
+}
